6e09615ce8b530a92656838db033fffa12671567
Carlos Santos <casantosmu@gmail.com>
2023-09-27 15:09:20 +0200

    Move subject creating to helper function

--- src/emailClient.ts
+++ src/emailClient.ts
@@ -10,34 +10,35 @@ const snsSubjectMaxLong = 100;
 
 const snsClient = new SNSClient({});
 
+const createSubjectFromRecord = (record: TRecord): string => {
+  const initialSubject = `Metal Tracker - New ${record.type}: ${record.title}`;
+  const cleanSubject = initialSubject.split("").reduce((result, character) => {
+    if (isAsciiCharacter(character) && !isControlCharacter(character)) {
+      return result + character;
+    }
+    return result;
+  });
+  return truncateString(cleanSubject, snsSubjectMaxLong);
+};
+
 export const sendRecordsEmail = async (
   records: TRecord[],
   topicArn: string,
 ): Promise<void> => {
   const promises = records.map(async (record) => {
-    const initialSubject = `Metal Tracker - New ${record.type}: ${record.title}`;
-    const cleanSubject = initialSubject
-      .split("")
-      .reduce((result, character) => {
-        if (isAsciiCharacter(character) && !isControlCharacter(character)) {
-          return result + character;
-        }
-        return result;
-      });
-    const finalSubject = truncateString(cleanSubject, snsSubjectMaxLong);
-
+    const subject = createSubjectFromRecord(record);
     const date = record.publicationDate.toLocaleDateString("es-ES", {
       weekday: "long",
       year: "numeric",
       month: "long",
       day: "numeric",
     });
-    const emailMessage = `A new ${record.type} has been published on ${record.sourceName}\n\nTitle: ${record.title}\nDate: ${date}\nDescription: ${record.description}\nLink: ${record.link}`;
+    const message = `A new ${record.type} has been published on ${record.sourceName}\n\nTitle: ${record.title}\nDate: ${date}\nDescription: ${record.description}\nLink: ${record.link}`;
 
     return snsClient.send(
       new PublishCommand({
-        Subject: finalSubject,
-        Message: emailMessage,
+        Subject: subject,
+        Message: message,
         TopicArn: topicArn,
       }),
     );
