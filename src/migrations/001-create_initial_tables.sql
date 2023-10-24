7b2cfe7d69b282a86d2846651d4f394001d6b2d3
Carlos Santos <casantosmu@gmail.com>
2023-10-25 00:15:46 +0200

    Add app integrity check

--- src/migrations/001-create_initial_tables.sql
+++ src/migrations/001-create_initial_tables.sql
@@ -3,16 +3,16 @@ CREATE TABLE record_types (type TEXT NOT NULL PRIMARY KEY);
 CREATE TABLE sources (source TEXT NOT NULL PRIMARY KEY);
 
 CREATE TABLE records (
+    record_id TEXT NOT NULL,
     type TEXT NOT NULL,
     source TEXT NOT NULL,
-    record_id TEXT NOT NULL,
     title TEXT NOT NULL,
     link TEXT NOT NULL,
     publication_date TEXT NOT NULL,
     description TEXT NOT NULL,
     updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
     created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(source, record_id),
+    PRIMARY KEY(record_id, source),
     FOREIGN KEY (type) REFERENCES record_types (type)
         ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (source) REFERENCES sources (source)
