382e6434af198175207fb724417f6146dd7d474e
Carlos Santos <casantosmu@gmail.com>
2023-10-25 17:19:32 +0200

    Add prettier to lint && Remove export buildUrl

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,25 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { buildUrl, subtractDays } from "../src/utils.js";
-
-describe("buildUrl", () => {
-  describe("when receives url: 'http://url.com/', path: '/some/path/' and params: '{ foo: 'bar', num: 2 }'", () => {
-    it("should return 'http://url.com/some/path?foo=bar&num=2'", () => {
-      const url = "http://url.com/";
-      const options = {
-        path: "/some/path",
-        params: {
-          foo: "bar",
-          num: 2,
-        },
-      };
-      const expectedResult = "http://url.com/some/path?foo=bar&num=2";
-
-      const result = buildUrl(url, options);
-
-      expect(result).toBe(expectedResult);
-    });
-  });
-});
+import { subtractDays } from "../src/utils.js";
 
 describe("subtractDays", () => {
   describe("when subtracting 7 days from 'May 27, 2009'", () => {
